5b63f4347af7e2b0da58a63af5e14c7c0b9f2e57
soty <websoty@gmail.com>
2025-12-09 18:11:18 +0200

    feat: profile UI/email editing state , bottom sheet UI

---
+++ components/BottomSheets/profileSheets.tsx
@@ -0,0 +1,18 @@
+import { Pressable, Text, View } from "react-native";
+
+
+
+export default function ProfileSheets() {
+  return (
+    <View className="bg-white-200 p-4 rounded-xl pb-10">
+      <Text className="text-4xl font-semibold mb-7">Settings</Text>
+      <Pressable className="bg-red-600 py-3 rounded-xl mb-3">
+        <Text className="text-white text-2xl text-center">Log out</Text>
+      </Pressable>
+      <Pressable className="bg-white border border-gray-400 py-3 rounded-xl">
+        <Text className="text-2xl text-center">Cancel</Text>
+      </Pressable>
+    </View>
+    
+  )
+}
